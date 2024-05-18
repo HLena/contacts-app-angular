3417097b2491baef0f00f56da4437cdeb17fbbfe
HLena <gordillo.vi20@gmail.com>
2024-05-18 12:07:12 -0500

    feat: Implement search functionality with RxJS Observables and operators - Add BehaviorSubject to manage search term input. -Use debounceTime and distincyUntilChanged operators to optimize search input handling. - Implement switchMap to handle search requests and update the filtered contacts list

--- src/app/shared/ui/components/search-contact/search-contact.component.ts
+++ src/app/shared/ui/components/search-contact/search-contact.component.ts
@@ -4,12 +4,13 @@ import { RouterModule } from '@angular/router';
 import { ContactService } from '../../../data-access/contact.service';
 import { Contact } from '../../../interface/contact.interface';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, Observable,  debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-search-contact',
   standalone: true,
-  imports: [RouterModule, HttpClientModule, CommonModule, ReactiveFormsModule],
+  imports: [RouterModule, HttpClientModule, CommonModule, FormsModule],
   templateUrl: './search-contact.component.html',
   styleUrl: './search-contact.component.css',
   providers: [ContactService]
@@ -17,20 +18,34 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 export class SearchContactComponent {
 
   public contacts: Contact[] = [];
-  public searchInput = new FormControl();
   public selectedContactId: null | string = null;
+  private searchTerms = new BehaviorSubject<string>('');
 
   constructor(private contactService: ContactService){}
 
   ngOnInit(): void {
-    this.contactService.getContacts()
-    .subscribe(contacts => this.contacts = contacts)
+    this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => this.searchContact(term))
+    ).subscribe(
+      results => this.contacts = results
+    )
   }
 
-  searchContact(){
-    const query = this.searchInput.value || '';
-    this.contactService.searchContactByName(query)
-      .subscribe(contacts => this.contacts = contacts)
+  searchContact(term: string): Observable<Contact[]>{
+    if(term === '') {
+      return this.contactService.getContacts();
+    }
+    return this.contactService.searchContactByName(term);
+
+  }
+
+  onSearchChange(event: Event){
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement) {
+      this.searchTerms.next(inputElement.value);
+    }
   }
 
   selectedContact(contactId: string):void {
