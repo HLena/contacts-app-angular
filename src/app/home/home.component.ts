412c99c372b6bf9fa91340547dd6702a33fb466e
HLena <gordillo.vi20@gmail.com>
2024-05-17 20:45:05 -0500

    refactor: Moved search functionality to a new 'searchContact' component and integrated it into 'home' component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Contact } from '../shared/interface/contact.interface';
 import { ContactService } from '../shared/data-access/contact.service';
-import { HttpClientModule } from '@angular/common/http';
+import { SearchContactComponent } from '../shared/ui/components/search-contact/search-contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, HttpClientModule],
+  imports: [RouterModule, SearchContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  providers: [ContactService]
 })
-export class HomeComponent implements OnInit{
-
-  public contacts: Contact[] = [];
-
-  constructor(private contactService: ContactService){}
-
-  ngOnInit(): void {
-    this.contactService.getContacts()
-    .subscribe(contacts => this.contacts = contacts)
-  }
-
+export class HomeComponent{
 }
