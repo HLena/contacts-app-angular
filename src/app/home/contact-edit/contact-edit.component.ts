ae3c5f7f72fd18cb718adc2dd854a90fa2063d5f
CrhistianT <andrewturpo573@gmail.com>
2024-05-18 21:46:42 -0500

    feat: edit functionality working

--- src/app/home/contact-edit/contact-edit.component.ts
+++ src/app/home/contact-edit/contact-edit.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Contact } from '../../shared/interface/contact.interface';
+import { switchMap } from 'rxjs';
+import { ContactService } from '../data-access/contact.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-edit',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './contact-edit.component.html',
-  styleUrl: './contact-edit.component.css'
+  styleUrl: './contact-edit.component.css',
+  providers: [ContactService]
 })
 export class ContactEditComponent {
+  userId: string = '';
+  contact: Contact | undefined;
+  formContactData: Contact = {} as Contact;
 
+  constructor(
+    private route: ActivatedRoute,
+    private contactService: ContactService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          this.userId = params.get('id') || '';
+          return this.contactService.getContactById(this.userId);
+        })
+      )
+      .subscribe((contact) => {
+        this.contact = contact;
+        this.formContactData = {...this.contact}
+      });
+  }
+
+  onSubmit(form: NgForm){
+    if(form.valid) {
+      this.contactService.updateContact(this.userId, this.formContactData).subscribe()
+      this.router.navigate(['/contact', this.userId])
+    }
+  }
 }
