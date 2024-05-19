3ced59d76c3fcf8071165f104fd7a7eafffb740a
HLena <gordillo.vi20@gmail.com>
2024-05-19 13:34:35 -0500

    feat: Refactor SearchContactComponent to manage state with BehaviorSubject - Updated to use BehaviorSubject for managing selected contact ID. - Added setupSearchSubscription, setupContactsSubscription, and setupSelectedContactSubscription methods for better readability. - Subscribed to selectedContactId$ observable. - Removed localStorage usage for storing the selected contact ID. - Ensured proper cleanup of subscriptions in ngOnDestroy.

--- src/app/home/ui/components/search-contact/search-contact.component.ts
+++ src/app/home/ui/components/search-contact/search-contact.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { ContactService } from '../../../data-access/contact.service';
 import { Contact } from '../../../../shared/interface/contact.interface';
 import { BehaviorSubject,  Subscription,  debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-contact',
@@ -18,28 +18,31 @@ export class SearchContactComponent implements OnInit, OnDestroy{
   public contacts: Contact[] = [];
   public selectedContactId: null | string = null;
   private searchTerms = new BehaviorSubject<string>('');
-  private subscription!: Subscription;
+  private subscription: Subscription = new Subscription();
 
   constructor(private contactService: ContactService){}
 
   ngOnInit(): void {
-    this.subscription = this.searchTerms.pipe(
+    this.subscription.add(this.setupSearchSubscription());
+    this.subscription.add(this.setupContactsSubscription());
+    this.subscription.add(this.setupSelectedContactSubscription());
+  }
+
+  private setupSearchSubscription(): Subscription{
+    return this.searchTerms.pipe(
       debounceTime(300),
       distinctUntilChanged(),
       switchMap(term => {
         this.contactService.searchContact(term)
         return this.contactService.contacts$
       })
-    ).subscribe(
-      results => this.contacts = results
-    )
-    this.subscription.add(
-      this.contactService.contacts$.subscribe(contacts => {
-        this.contacts = contacts;
-      })
-    )
+    ).subscribe(contacts => this.contacts = contacts)
+  }
 
-    this.subscription.add(this.setupSelectedContactSubscription());
+  private setupContactsSubscription(): Subscription{
+    return this.contactService.contacts$.subscribe(contacts => {
+      this.contacts = contacts;
+    })
   }
 
   private setupSelectedContactSubscription(): Subscription {
@@ -61,9 +64,7 @@ export class SearchContactComponent implements OnInit, OnDestroy{
   }
 
   ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.subscription.unsubscribe();
   }
 
 }
