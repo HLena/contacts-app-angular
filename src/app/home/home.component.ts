d2e236bfd69d91dabbece5ebfb3af07e9cee6fc3
HLena <gordillo.vi20@gmail.com>
2024-05-18 21:09:32 -0500

    feat: Add HttpClientModule import to enable HTTP requests in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ContactService } from './data-access/contact.service';
 import { SearchContactComponent } from './ui/components/search-contact/search-contact.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, SearchContactComponent],
+  imports: [RouterModule, SearchContactComponent, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
